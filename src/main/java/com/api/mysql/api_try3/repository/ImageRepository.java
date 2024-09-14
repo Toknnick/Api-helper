87578dbdc0b18191b8d5405899e05c51d8f463ce
Toknnick <Toknnick@yandex.ru>
2024-09-14 08:43:55 +0300

    мелкий фикс

--- src/main/java/com/api/mysql/api_try3/repository/ImageRepository.java
+++ src/main/java/com/api/mysql/api_try3/repository/ImageRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
     @Query("SELECT i FROM Image i WHERE i.idRoom = :idRoom AND i.date = :date AND i.time = :time AND i.imageData = :imageData AND i.imageType = :imageType")
-    Image findImageByCriteria(@Param("idRoom") Long idRoom, @Param("date") String date, @Param("time") String time, @Param("imageData") byte[] imageData, @Param("imageType") String imageType);
+    Image findImageByCriteria(@Param("idRoom") Long idRoom, @Param("date") String date, @Param("time") String time, @Param("imageData") String imageData, @Param("imageType") String imageType);
 
     List<Image> findByIdRoom(Long idRoom);
 }
